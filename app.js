8a6a6d0d257d64781a86df57e0cfe93431d66e87
Julie Santiago <santiago.sjulie@gmail.com>
2020-02-05 00:34:11 -0300

    Cria requisições para o banco de dados mysql

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 let app = require ('./config/server')//módulo que contém as configurações do servidor 
 
-app.listen(3000, function(){
-    console.log ('Servidor rodando com Express')
+app.listen(4000, function(){
+    console.log ('Server ON'); 
 }); 
-/*Bug: Nodemon não está assinado digitalmente - não pude executá-lo*/ 
-
-/*let home = require('./app/routes/home'); 
-home(app); */ 
-//MELHOR FORMA, segundo o CommonJS: 
 
 let home = require('./app/routes/home')(app); 
  
-let formulario = require ('./app/routes/formulario'); //Este módulo é uma função, é preciso chamá-la abaixo 
+let formulario = require ('./app/routes/formulario'); 
 formulario(app); 
 
 
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,21 @@
-module.exports = function (app){ //app é o objeto criado lá na config do servidor
-    app.get ('/produtos', function(request, response){
-        response.render ('produtos/produtos.ejs') //Com o EJS, o Express passa a ter o método RENDER
+module.exports = function (app){ 
+
+app.get ('/produtos', function(request, response){    
+        let mysql = require ('mysql'); //módulo já instalado
+
+        let conexao = mysql.createConnection ({     
+            host : 'localhost', //parâmetros são passados como estrutura JSON
+            user: 'root', 
+            password: '1234', 
+            database: 'atelier'
+        })
+
+        conexao.query('select * from posts', function(error, result){ //dois parametros: erro e resultado  
+            //após a consulta em si, passamos uma função de callback 
+            //response.render('produtos/produtos.ejs', {posts : result}) 
+            response.send(result); 
+            
+        }); 
     })
-}//Colocando dentro de uma função porque é preciso encapsular os comandos acima 
-//No módulo só é possível enviar uma coisa de cada vez 
+}
+
