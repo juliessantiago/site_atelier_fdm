94df260098c5b4f7319c81badf8fcc838a7ec0d5
Julie Santiago <santiago.sjulie@gmail.com>
2020-04-08 18:15:41 -0300

    Adiciona módulo Express Validator

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require ('body-parser'); 
+var expressValidator = require('express-validator'); 
 
 var app = express();
 
@@ -8,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true})) //permite que sejam usadas URLs codificadas
+app.use(expressValidator()); //O módulo exporta uma função e aqui já estamos executando-a 
 
 consign()
     .include('app/routes') //Carrega automaticamente todas as rotas 
